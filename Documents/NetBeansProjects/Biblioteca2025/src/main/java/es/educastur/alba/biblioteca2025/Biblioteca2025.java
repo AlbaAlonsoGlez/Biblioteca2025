a14624344605c715b828dbbf9854838af91db069
Alba <albaalonsoglez@gmail.com>
2024-12-11 14:02:41 +0100

    Proyecto Biblioteca2025 vacío; solo clases VO

---
+++ Documents/NetBeansProjects/Biblioteca2025/src/main/java/es/educastur/alba/biblioteca2025/Biblioteca2025.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.educastur.alba.biblioteca2025;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alu01d
+ */
+public class Biblioteca2025 {
+
+    private ArrayList <Libro> libros;
+    private ArrayList <Usuario> usuarios;
+    private ArrayList <Prestamo> prestamos;
+    
+    public Biblioteca2025() {
+        this.libros = new ArrayList();
+        this.usuarios = new ArrayList();
+        this.prestamos = new ArrayList();
+    }
+    
+    public static void main(String[] args) {
+        Biblioteca2025 b= new Biblioteca2025();
+        b.cargaDatos();
+    }
+    
+    private void cargaDatos() {
+        
+    }
+    
+    private void menu() {
+        
+    }
+    
+}
